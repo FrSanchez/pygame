78885a16c798117c9bc6411341912499ff9f891e
Francisco Sanchez <francisco.sanchez@salesforce.com>
2021-02-08 08:16:49 -0800

    Added map sample

--- map.py
+++ map.py
@@ -1,99 +1,75 @@
-# This code is in the Public Domain
-# -- richard@mechanicalcat.net
+#! /usr/bin/env python
 
-import math, os, pygame, random, pytmx
-from pygame.locals import *
+from __future__ import division, print_function, unicode_literals
 
-class Map:
-    def __init__(self, map, tiles):
-        self.tiles = pygame.image.load(tiles)
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
-        l = [line.strip() for line in open(map).readlines()]
-        self.map = [[None]*len(l[0]) for j in range(len(l))]
-        for i in range(len(l[0])):
-            for j in range(len(l)):
-                tile = l[j][i]
-                tile = tile_coords[tile]
-                if tile is None:
-                    continue
-                elif isinstance(tile, type([])):
-                    tile = random.choice(tile)
-                cx, cy = tile
-                if random.choice((0,1)):
-                    cx += 192
-                if random.choice((0,1)):
-                    cy += 192
-                self.map[j][i] = (cx, cy)
+from pyglet.gl import *
+from pyglet.window import key
 
-    def draw(self, view, viewpos):
-        '''Draw the map to the "view" with the top-left of "view" being at
-           "viewpos" in the map.
-        '''
-        sx, sy = view.get_size()
-        bx = viewpos[0]/64
-        by = viewpos[1]/64
-        for x in range(0, sx+64, 64):
-            i = x/64 + bx
-            for y in range(0, sy+64, 64):
-                j = y/64 + by
-                try:
-                    tile = self.map[j][i]
-                except IndexError:
-                    # too close to the edge
-                    continue
-                if tile is None:
-                    continue
-                cx, cy = tile
-                view.blit(self.tiles, (x, y), (cx, cy, 64, 64))
-
-    def limit(self, view, pos):
-        '''Limit the "viewpos" variable such that it defines a valid top-left
-           rectangle of "view"'s size over the map.
-        '''
-        x, y = pos
-        # easy
-        x = max(x, 0)
-        y = max(y, 0)
-
-        # figure number of tiles in a view, hence max x and y viewpos
-        sx, sy = view.get_size()
-        nx, ny = sx/64, sy/64
-        mx = (len(self.map[0]) - nx) * 64
-        my = (len(self.map) - ny) * 64
-        print(y, my)
-
-        return (min(x, mx), min(y, my))
-
-def main():
-    pygame.init()
-    win = pygame.display.set_mode((960, 640))
-
-    map = pytmx.TiledMap('data/ground.tmx')
-    layer = map.layers[0]
-    viewpos = (0,0)
-    move = False
-    clock = pygame.time.Clock()
-    sx, sy = win.get_size()
-    while 1:
-        event = pygame.event.poll()
-        while event.type != NOEVENT:
-            if event.type in (QUIT, KEYDOWN):
-                sys.exit(0)
-            elif event.type == MOUSEBUTTONDOWN:
-                x, y = viewpos
-                dx, dy = event.pos
-                x += dx - sx/2
-                y += dy - sy/2
-                viewpos = map.limit(win, (x, y))
-                move = True
-            event = pygame.event.poll()
-
-        win.fill((0,0,0))
-        # map.draw(win, viewpos)
-        pygame.display.flip()
-
-        clock.tick(30)
+from cocos.actions import *
+from cocos.director import director
+from cocos.layer import Layer
+from cocos.scene import Scene
+from cocos.sprite import Sprite
 
 
-if __name__ == '__main__':
-    main()
+class SpriteLayer(Layer):
+
+    is_event_handler = True     #: enable pyglet's events
+
+    def __init__(self, index=1):
+        super(SpriteLayer, self).__init__()
+        self.index = index
+
+        self.image = pyglet.resource.image('grossini.png')
+        self.image.anchor_x = self.image.width // 2
+        self.image.anchor_y = self.image.height // 2
+
+        self.image_sister1 = pyglet.resource.image('grossinis_sister1.png')
+        self.image_sister1.anchor_x = self.image_sister1.width // 2
+        self.image_sister1.anchor_y = self.image_sister1.height // 2
+
+        self.image_sister2 = pyglet.resource.image('grossinis_sister2.png')
+        self.image_sister2.anchor_x = self.image_sister2.width // 2
+        self.image_sister2.anchor_y = self.image_sister2.height // 2
+
+    def on_key_release(self, keys, mod):
+        # LEFT: go to previous scene
+        # RIGTH: go to next scene
+        # ENTER: restart scene
+        if keys == key.LEFT:
+            self.index -= 1
+            if self.index < 1:
+                self.index = len(tests)
+        elif keys == key.RIGHT:
+            self.index += 1
+            if self.index > len(tests):
+                self.index = 1
+
+        if keys in (key.LEFT, key.RIGHT, key.ENTER):
+            director.replace(get_sprite_test(self.index))
+            return True
+
+    # def on_exit( self ):
+    #    for o in self.objects:
+    #        o.stop()
+
+
+class SpriteMoveTo(SpriteLayer):
+
+    def on_enter(self):
+        super(SpriteMoveTo, self).on_enter()
+
+        sprite3 = Sprite(self.image)
+        self.add(sprite3)
+        sprite3.position = 320, 300
+        sprite3.do(MoveTo((620, 300), 4))
+
+
+if __name__ == "__main__":
+    director.init(resizable=True, caption='Cocos - Sprite demo')
+#    director.window.set_fullscreen(True)
+    director.run(Scene(SpriteMoveTo(1)))
--- testtmx.py
+++ testtmx.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python
-
 from __future__ import division, print_function, unicode_literals
 
 # This code is so you can run the samples without installing the package
@@ -20,21 +18,38 @@ pyglet.resource.reindex()
 import cocos
 from cocos import tiles, actions, layer
 
-  
+class DriveCar(actions.Driver):
+    def step(self, dt):
+        # handle input and move the car
+        self.target.rotation += (keyboard[key.RIGHT] - keyboard[key.LEFT]) * 150 * dt
+        self.target.acceleration = (keyboard[key.UP] - keyboard[key.DOWN]) * 400
+        if keyboard[key.SPACE]: self.target.speed = 0
+        super(DriveCar, self).step(dt)
+        scroller.set_focus(self.target.x, self.target.y)
+
 def main():
     global keyboard, scroller
     from cocos.director import director
-    director.init(width=960, height=640, autoscale=False, resizable=True)
+    director.init(width=800, height=600, autoscale=False, resizable=True)
+
+    car_layer = layer.ScrollableLayer()
+    car = cocos.sprite.Sprite('car.png')
+    car_layer.add(car)
+    car.position = (200, 100)
+    car.max_forward_speed = 200
+    car.max_reverse_speed = -100
+    car.do(DriveCar())
 
     scroller = layer.ScrollingManager()
     test_layer = tiles.load('road-map.tmx')['map0']
     scroller.add(test_layer)
+    scroller.add(car_layer)
 
     main_scene = cocos.scene.Scene(scroller)
-    
 
     keyboard = key.KeyStateHandler()
     director.window.push_handlers(keyboard)
+
     def on_key_press(key, modifier):
         if key == pyglet.window.key.Z:
             if scroller.scale == .75:
@@ -43,8 +58,6 @@ def main():
                 scroller.do(actions.ScaleTo(.75, 2))
         elif key == pyglet.window.key.D:
             test_layer.set_debug(True)
-        
-        
     director.window.push_handlers(on_key_press)
 
     director.run(main_scene)
